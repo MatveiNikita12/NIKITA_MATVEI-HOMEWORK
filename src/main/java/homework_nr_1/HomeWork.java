d429c0a3727bc45d78b72b86496931f0eb46b354
Nikita Matvei <127681861+MatveiNikita12@users.noreply.github.com>
2023-03-18 19:46:31 +0200

    HomeWork

---
+++ src/main/java/homework_nr_1/HomeWork.java
@@ -0,0 +1,24 @@
+package homework_nr_1;
+
+public class HomeWork {
+    public static void main(String[] args) {System.out.println("Hello world!");
+
+        System.out.println();
+        // чтобы между разными заданиями был отступ
+        long Current_World_Population = 7811080815L;
+        int Cars_Produced_This_Year = 55235358;
+        char First_Letter_In_Alphabet = 'A';
+        boolean Java_Is_Amazing = true;
+        float Pi = 3.14159F;
+         System.out.println(Current_World_Population + "\n" + Cars_Produced_This_Year + "\n" + First_Letter_In_Alphabet + "\n" + Java_Is_Amazing + "\n" + Pi);
+
+         System.out.println();
+
+         double Price = 3.546;
+         double Tax = 4.643;
+         int Quantity = 275627;
+         double Sum = 0;
+         double total = Price + Tax * Quantity + Sum;
+         System.out.println(total);
+    }
+}
