8b5cf9d2e43d883fcae5601a0dd52f6a219843fe
Nikita Matvei <127681861+MatveiNikita12@users.noreply.github.com>
2023-04-19 20:35:55 +0300

    HomeWork5

--- src/main/java/homework_nr_5/Months.java
+++ src/main/java/homework_nr_5/Months.java
@@ -1,7 +1,7 @@
 package homework_nr_5;
 
 public enum Months {
-    January, February, March, April, May, June, July, September, October, November, December;
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
 }
 
 
