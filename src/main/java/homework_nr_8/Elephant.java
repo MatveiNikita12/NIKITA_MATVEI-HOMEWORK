f0f5d2865c16ec9ef5c0fd181637b192c20e2992
Nikita Matvei <127681861+MatveiNikita12@users.noreply.github.com>
2023-05-10 17:48:33 +0300

    HomeWork8

---
+++ src/main/java/homework_nr_8/Elephant.java
@@ -0,0 +1,24 @@
+package homework_nr_8;
+
+public class Elephant extends Erdivor{
+
+    @Override
+    public void voice() {
+        System.out.println("Elephant's voice is 'vrrrrrrr' ");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Elephant can run and swim");
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void doSomeThing() {
+
+    }
+}
---
+++ src/main/java/homework_nr_8/test_hm8.java
@@ -0,0 +1,14 @@
+package homework_nr_8;
+
+public class test_hm8 {
+    public static void main(String[] args) {
+        Animal animal = new Elephant();
+        Erdivor erdivor = new Elephant();
+        erdivor.parentsClass();
+        erdivor.doSomeThing();
+        erdivor.eat();
+        erdivor.move();
+        erdivor.voice();
+
+    }
+}
